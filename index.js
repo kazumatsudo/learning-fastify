2084347be65f5af087c3afe9f19574bc9c170cf3
Kazuma Matsudo <matsudo.kazuma@unipro.co.jp>
2018-11-23 11:18:57 +0900

    index.js async/await を用いたパターンに変更

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ const fastify = require('fastify')({
 });
 
 // ルーティングの設定
-fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'world' })
+// reply.send を用いずに return で返すことも可能
+fastify.get('/', async () => {
+    return { hello: 'world' };
 });
 
 // ポート3000番で受ける
-fastify.listen(3000, function (err, address) {
-    if (err) {
+const start = async () => {
+    try {
+        await fastify.listen(3000);
+    } catch (err) {
         fastify.log.error(err);
         process.exit(1);
     }
-    fastify.log.info(`server listening on ${address}`)
-});
+};
+
+// 実行
+start();
